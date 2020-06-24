cb0c3ecdf45311d765a586c4d83e9b656c94ce4f
yailen fajardo <yailenfajardoegues@gmail.com>
2020-06-24 18:30:44 -0300

    modificando formulario

--- client/src/components/global/Contacto.jsx
+++ client/src/components/global/Contacto.jsx
@@ -2,19 +2,26 @@ import React from "react";
 import "./Contacto.css";
 
 function Contacto() {
-  return (
-    <div>
-      <form action="">
-        <label htmlFor="name">Nombre</label><br/>
-        <input type="text" name="nombre"/><br/>
-        <label htmlFor="edad">Edad</label><br/>
-        <input type="nomber" name="edad" /><br/>
-        <label htmlFor="email">correo</label><br/>
-        <input type="email" name="correo" /><br/>
-        <input type="submit"  value="submit"/><br/>
-      </form>
-    </div>
-  );
+    return (
+        <div className="container">
+            <form action="">
+                <label className="label" htmlFor="name">Nombre</label>
+                <br />
+                <input className="input" type="text" name="nombre" />
+                <br />
+                <label className="label" htmlFor="email">Correo</label>
+                <br />
+                <input className="input" type="email" name="correo" />
+                <br />
+                <label className="label" htmlFor="email">Coméntanos</label>
+                <br />
+                <textarea className="inputo" name="" id="texto"  ></textarea>
+                <br />
+                <input  className="button" type="submit" value="Enviar" />
+                <br />
+            </form>
+        </div>
+    );
 }
 
 export default Contacto;
